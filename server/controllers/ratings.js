7b3c7c82b782d1b8f91f358d1310808a56cc11cf
Toan Tran <38516119+songtoan0201@users.noreply.github.com>
2020-01-24 14:25:16 -0800

    inital commit

--- server/controllers/ratings.js
+++ server/controllers/ratings.js
@@ -1,70 +1,58 @@
-let mongoose = require('mongoose'),
-    Cake = mongoose.model('Cake'),
-    Rating = mongoose.model('Rating');
+let mongoose = require("mongoose"),
+  Cake = mongoose.model("Cake"),
+  Rating = mongoose.model("Rating");
 
 module.exports = {
+  index: (req, res) => {
+    Rating.find()
+      .populate("_cake")
+      .exec()
+      .then(allR => {
+        res.json(allR);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  },
 
-    index: (req,res) =>{
-        Rating.find().populate('_cake').exec()
-            .then(allR=>{
-                res.json(allR)
-            })
-            .catch(err=>{
+  create: function(req, res) {
+    // create the newest object
+    let newestRating = new Rating(req.body);
+
+    // add an attribute _cake, the parent to the newest object
+    newestRating._cake = req.params.cakeId;
+
+    // below is an acyncrous call to the database
+    // thus we need to have a try/catch
+    newestRating
+      .save()
+      .then(saveRating => {
+        Cake.findOne({ _id: req.params.cakeId })
+          .then(theCake => {
+            theCake._ratings.push(saveRating);
+            // now save that cake
+            theCake
+              .save() // this is a trip to the database
+              .then(savedCake => {
+                // output the orginal rating that we just created
+                // after all the million joins
+                res.json(newestRating);
+              })
+              .catch(err => {
                 res.json(err);
-            })
-    },
-
-    create: function (req, res) {
-        // create the newest object
-        let newestRating = new Rating(req.body);
-
-        // add an attribute _cake, the parent to the newest object
-        newestRating._cake = req.params.cakeId;
-
-        // below is an acyncrous call to the database
-        // thus we need to have a try/catch
-        newestRating.save()
-            .then(saveRating => {
-                console.log("saving the " + saveRating);
-
-                // if the rating is saveable
-                // find it's parent and join them
-                Cake.findOne({ _id: req.params.cakeId })
-                    .then(theCake => {
-                        console.log(`****cake found and saviing ${saveRating}  to the cake ${theCake}`);
-                        theCake._ratings.push(saveRating);
-                        console.log(theCake._ratings)
-
-                        // now save that cake
-                        theCake.save() // this is a trip to the database
-                            .then(savedCake => {
-                                console.log("******saved the cake succesfully")
-                                // output the orginal rating that we just created 
-                                // after all the million joins
-                                res.json(newestRating);
-                            })
-                            .catch(err => {
-                                console.log("errored on the last try");
-                                res.json(err);
-                            })
-                    })
-                    .catch(err => {
-                        console.log("errored on the second try");
-                        res.json(err);
-                    })
-            })
-            .catch(err =>{
-                console.log("errored on the first try");
-                res.json(err);
-            })
-    },
-    findAllRatings: (req,res)=>{
-
-    },
-    editRating: (req,res) =>{ // req.body, {runValidtor: true}
-
-    },
-    deleteRating: (req,res) =>{
-
-    }
-}
+              });
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  },
+  findAllRatings: (req, res) => {},
+  editRating: (req, res) => {
+    // req.body, {runValidtor: true}
+  },
+  deleteRating: (req, res) => {}
+};
